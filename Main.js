835422675e87baeac7389e1197c7833554d5ce6b
darthpaul <paulakhamiogu@gmail.com>
2021-03-28 21:11:16 +0100

    Add local store, omo, you go fix the bug yourself oo

--- Main.js
+++ Main.js
@@ -1,12 +1,20 @@
+const TODOS_KEY = "todos";
 const input = document.getElementById("addText");
 const btn = document.getElementById("addBtn");
 const list = document.getElementById("list");
 const todo = [];
 
+
 function addTodo() {
   if(input.value){
     todo.push(input.value.trim());
     input.value = "";
+    displayOnUi();
+    window.localStorage.setItem(TODOS_KEY, JSON.stringify(todo));
+  }
+}
+
+function displayOnUi(){
   const listItem = document.createElement("div");
   listItem.setAttribute("class", "item");
   
@@ -33,17 +41,24 @@ function addTodo() {
   
   for (var i = 0; i < todo.length; i++) {
     listItemText.innerHTML = todo[i];
-    
+
     listItem.append(listItemCheck);
     listItem.append(listItemText);
     listItem.append(listItemDel);
     list.prepend(listItem);
   }
+
   const refresh = document.getElementById("refresh");
   refresh.addEventListener("click", () => {
     listItem.style.display = "none";
   });
-  }
+}
+
+window.onload = function loadSavedData(){
+  const savedTodos = JSON.parse(window.localStorage.getItem(TODOS_KEY))
+  todo.push.apply(todo, savedTodos)
+  console.log(todo);
+  displayOnUi()
 }
 
 
